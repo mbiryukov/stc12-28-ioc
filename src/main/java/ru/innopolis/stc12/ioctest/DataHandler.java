76c6433becb3cef08a87476a5159ffd08c806f88
MVBiryukov-stc11 <m.biryukov@innopolis.ru>
2018-10-19 20:29:04 +0300

    XML-confiruration

--- src/main/java/ru/innopolis/stc12/ioctest/DataHandler.java
+++ src/main/java/ru/innopolis/stc12/ioctest/DataHandler.java
@@ -4,6 +4,9 @@ public class DataHandler {
     private Downloader downloader;
     private Uploader uploader;
 
+    public DataHandler() {
+    }
+
     public DataHandler(Downloader downloader, Uploader uploader) {
         this.downloader = downloader;
         this.uploader = uploader;
@@ -13,6 +16,14 @@ public class DataHandler {
         String content = this.downloader.download(srcPath);
         String handledContent = handle(content);
         this.uploader.upload(destPath, handledContent);
+    }
+
+    public void setDownloader(Downloader downloader) {
+        this.downloader = downloader;
+    }
+
+    public void setUploader(Uploader uploader) {
+        this.uploader = uploader;
     }
 
     private String handle(String content){
--- src/main/java/ru/innopolis/stc12/ioctest/Main.java
+++ src/main/java/ru/innopolis/stc12/ioctest/Main.java
@@ -1,17 +1,15 @@
 package ru.innopolis.stc12.ioctest;
 
-import javafx.application.Application;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.innopolis.stc12.ioctest.DBDownloader;
-import ru.innopolis.stc12.ioctest.DataHandler;
-import ru.innopolis.stc12.ioctest.FileUploader;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
-        DataHandler dataHandler = (DataHandler) context.getBean("dataHandler");
-
+        DataHandler dataHandler = (DataHandler) context.getBean(DataHandler.class);
+        DataHandler dataHandler2 =
+                (DataHandler) new ClassPathXmlApplicationContext("appContext.xml").getBean(DataHandler.class);
+        System.out.println("equals by ref: " + (dataHandler == dataHandler2));
         dataHandler.handleData("someUrl", "SomeOtherUrl");
     }
 }
